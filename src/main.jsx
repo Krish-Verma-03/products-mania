9423d8652fa7bd15f3415349627bf5c63d2070d4
Science-lover-krish <93467650+Science-lover-krish@users.noreply.github.com>
2024-07-07 21:05:56 +0530

    deployed

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Context>
-  <BrowserRouter>
+  <BrowserRouter basename="/products-mania/">
     <App />
     <ToastContainer />
   </BrowserRouter>
